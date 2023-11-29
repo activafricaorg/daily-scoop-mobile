3b95593a7edb2b99937bd8b71dcebe013306767e
Omeiza Owuda <owudaomeiza@gmail.com>
2023-11-29 01:03:53 +0100

    Country data constant

--- state/store/index.ts
+++ state/store/index.ts
@@ -3,10 +3,9 @@ import { persistStore, persistReducer } from 'redux-persist';
 import countryReducer from '../store/reducers/country';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const persistedReducer = persistReducer({ key: 'root', storage: AsyncStorage }, countryReducer) as typeof countryReducer
+const STORAGE_KEY = 'countryData';
+const persistedReducer = persistReducer({ key: STORAGE_KEY, storage: AsyncStorage }, countryReducer) as typeof countryReducer
 let store = createStore(persistedReducer);
 
 export default store;
 export const persistor = persistStore(store);
-export type AppDispatch = typeof store.dispatch
-export type RootState = ReturnType<typeof store.getState>
