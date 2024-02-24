bded0de2dd4340bb489270228509b14bc7182f9b
Omeiza Owuda <owudaomeiza@gmail.com>
2024-02-24 01:14:12 +0100

    Fix to network outage

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,26 @@
 import { ScrollView, Text, View } from "react-native";
 import baseStyles from "../styles/Base";
-import checkConnection from "../util/checkConnection";
 import { MaterialIcons } from '@expo/vector-icons';
+import {useEffect, useState} from "react";
+import NetInfo from "@react-native-community/netinfo";
+
+let currentNetwork: boolean | null;
+NetInfo.fetch().then((state) => {
+	currentNetwork = state.isConnected;
+});
 
 export default function Layout (props: {children: string | JSX.Element | JSX.Element[] | any}) {
-	let network = checkConnection();
+	const [connected, setConnected] = useState<boolean | null>(currentNetwork);
 
-	if (!network) return (
+	useEffect(() => {
+		const unsubscribe = NetInfo.addEventListener((state) => {
+			setConnected(state.isConnected);
+		});
+
+		return () => unsubscribe();
+	}, []);
+
+	if (!connected) return (
 		<View style={{flex: 1, justifyContent: "center", backgroundColor: '#0f0f0f', alignItems: 'center', paddingLeft: 20, paddingRight: 20}}>
 			<MaterialIcons name="wifi-off" size={48} color={"#646464"} style={{marginBottom: 10}} />
 			<Text style={{fontFamily: 'Aeonik-Medium', fontSize: 24, color: '#646464', marginBottom: 3, letterSpacing: 0.5}}>Connection Error</Text>
